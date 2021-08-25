fbc5333813e616b0d74bc0978021db9e0bd24e28
Kyle Garcia <kyle.fire@gmail.com>
2021-08-25 14:40:39 +0900

    almost finished mobile styling. still need color overlay

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -4,28 +4,36 @@ export default function StatsCard() {
   return (
     <div className='app-container'>
       <div className='card-container'>
-        <div className='hero-image'>
-          <img src={mobileHero} alt='image-mobile' />
+        <div className='hero-image-container'>
+          <div className='hero-overlay'></div>
+          <div>
+            <img className='hero-image' src={mobileHero} alt='image-mobile' />
+          </div>
         </div>
-        <h2 className='title'>Get insights that help your business grow.</h2>
-        <div className='description'>
-          <p>
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency.
-          </p>
+        <div className='text-container'>
+          <h2 className='main-heading'>
+            Get <span className='accent-color'>insights</span> that help your
+            business grow.
+          </h2>
+          <div className='main-paragraph'>
+            <p>
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+          <section className='companies-container'>
+            <div className='companies-amount stats'>10k+</div>
+            <div className='stats-heading'>COMPANIES</div>
+          </section>
+          <section className='templates-container'>
+            <div className='templates-amount stats'>314</div>
+            <div className='stats-heading'>TEMPLATES</div>
+          </section>
+          <section className='queries-container'>
+            <div className='queries-amount stats'>12M+</div>
+            <div className='stats-heading'>QUERIES</div>
+          </section>
         </div>
-        <section className='companies-container'>
-          <div className='companies-amount'>10k+</div>
-          <div className='companies-title'>COMPANIES</div>
-        </section>
-        <section className='templates-container'>
-          <div className='templates-amount'>314</div>
-          <div className='templates-title'>TEMPLATES</div>
-        </section>
-        <section className='queries-container'>
-          <div className='queries-amount'>12M+</div>
-          <div className='queries-title'>QUERIES</div>
-        </section>
       </div>
     </div>
   );
